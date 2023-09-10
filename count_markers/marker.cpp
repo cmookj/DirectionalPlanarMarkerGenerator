b714e4772b197fe5202397ada7c3d8f7e067d552
Changmook Chun <cmookj@gmail.com>
2023-09-10 08:05:35 -0700

    Added .gitignore

---
+++ count_markers/marker.cpp
@@ -0,0 +1,8 @@
+//
+//  marker.cpp
+//  count_markers
+//
+//  Created by Changmook Chun on 9/8/23.
+//
+
+#include "marker.hpp"
---
+++ count_markers/marker.hpp
@@ -0,0 +1,13 @@
+//
+//  marker.hpp
+//  count_markers
+//
+//  Created by Changmook Chun on 9/8/23.
+//
+
+#ifndef marker_hpp
+#define marker_hpp
+
+#include <stdio.h>
+
+#endif /* marker_hpp */
